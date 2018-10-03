74aaea77c115044a80c21dfe63abf7002c926030
Kylep342 <kylep342@gmail.com>
2018-10-03 07:02:39 -0500

    Fixes Removed default_locale in manifest until implementation is figured out Expanded regex to avoid modifying URLs

--- theAnswer.js
+++ theAnswer.js
@@ -1,4 +1,5 @@
 (function theAnswer() {
-  const re = /(\W)(Artificial Intelligence|AI|A\.I\.)(\W)?/gi;
+  // custom regex beyond \W to avoid messing with hrefs in URLs
+  const re = /([^a-zA-Z0-9_\/\?=])(Artificial Intelligence|AI|A\.I\.)([^a-zA-Z0-9_\/\?=])?/gi;
   document.body.innerHTML = document.body.innerHTML.replace(re, '$1Allen Iverson$3');
 })()
