d446b72dcd6d9d03de8b9455db72a3866023caf7
Kyle Pekosh <=>
2018-10-16 13:10:05 -0500

    update

--- theAnswer.js
+++ theAnswer.js
@@ -1,5 +1,24 @@
-(function theAnswer() {
-  // custom regex beyond \W to avoid messing with hrefs in URLs
-  const re = /([^a-zA-Z0-9_\/\?=])(Artificial Intelligence|AI|A\.I\.)([^a-zA-Z0-9_\/\?=])?/gi;
-  document.body.innerHTML = document.body.innerHTML.replace(re, '$1Allen Iverson$3');
-})()
+// function randomAI(match, p1, p2, p3, offset, string) {
+// 	const nicknames = ['The Answer', 'Bubba Chuck', 'Jewelz', 'The Third Degree', 'Allen Iverson'];
+// 	let choice = nicknames[Math.floor(Math.random() * nicknames.length)];
+// 	return [p1, choice, p3].join('');
+// }
+
+let elements = document.getElementsByTagName('*');
+
+for (let i = 0; i < elements.length; i++) {
+	element = elements[i];
+
+	for (let j = 0; j < element.childNodes.length; j++) {
+		let node = element.childNodes[j];
+
+		if (node.nodeType === 3) {
+			let text = node.nodeValue;
+			let replacedText = text.replace(/(\W|^)(Artificial Intelligence|AI|A\.I\.)(\W|$)/gi, '$1Allen Iverson$3');
+
+			if (replacedText !== text) {
+				element.replaceChild(document.createTextNode(replacedText), node);
+			}
+		}
+	}
+}
